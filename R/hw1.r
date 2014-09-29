6e98d122f75c1fe38903380e7912816398ab3ec8
Gabor Csardi <csardi.gabor@gmail.com>
2014-09-29 01:31:57 -0400

    9 param combinations for noisy-cor

--- R/hw1.r
+++ R/hw1.r
@@ -7,8 +7,17 @@
 #SBATCH --mem-per-cpu=4096 # Memory required per CPU, in MegaBytes
 #SBATCH --mail-user=csardi.gabor@gmail.com
 #SBATCH --mail-type=ALL    # When to send mail
+#SBATCH -a 1-9             # Array of 9 jobs, with ids 1, 2, ..., 9
 
 if (Sys.getenv("SLURM_JOB_ID") != "") {
   library(stat221)
-  noisy_cor(5000, 0.9, 0.1, to_file = TRUE)
+
+  true_cor <- c(0.5, 0.8, 0.9)
+  noise_var <- c(0.1, 0.2, 0.5)
+  my_id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+  params <- expand.grid(true_cor = true_cor, noise_var = noise_var)
+  my_params <- params[my_id,]
+  noisy_cor(num_genes = 5000, true_cor = my_params$true_cor,
+            noise_var = my_params$noise_var, to_file = TRUE)
+
 }
